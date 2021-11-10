a930957314974790c2790b48e79a4aa958527bc7
s-pardox <stefanopardini@inventati.org>
2021-11-10 11:34:09 +0100

    Discriminator model, as described in Tzeng et al. paper

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .lenet import LeNetEncoder, LeNetClassifier
 from .phase1 import Phase1Model
+from .discriminator import Discriminator
 
-__all__ = (LeNetEncoder, LeNetClassifier, Phase1Model)
+__all__ = (LeNetEncoder, LeNetClassifier, Phase1Model, Discriminator)
---
+++ models/discriminator.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+
+"""
+Discriminator model cues from Tzeng et al., page 6:
+
+"When training with ADDA, our adversarial discriminator consists of 3 fully connected layers: two layers with 500
+hidden units followed by the final discriminator output. Each of the 500-unit layers uses a ReLU activation function."
+"""
+
+
+class Discriminator(Model):
+    def __init__(self, input_shape):
+        super(Discriminator, self).__init__(trainable=True)
+
+        """
+        Input layer: [4 x 4 x 50]
+        The input layer shape should be equals to the output shape of the last encoder layer (pool_2)
+        """
+        input_shape = (4, 4, 50)
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=input_shape)
+
+        # Fully connected layers
+        self.full_layer_1 = tf.keras.layers.Dense(units=500, activation='relu')
+        self.full_layer_2 = tf.keras.layers.Dense(units=500, activation='relu')
+
+        """
+        TODO.
+        The last layer doesn't need an activation function (implicitly used a linear output activation function).
+        
+        Here we're not sure about the number of neurons it should be used.
+        Tzeng TensorFlow 1.x implementation and PyTorch ADDA implementation both use 2 neurons at the final layer.
+        
+        But, theoretically, we just need one neuron at the final layer, because we are producing a binary
+        classification:
+        0: Source CNN
+        1: Target CNN
+        """
+        self.full_layer_3 = tf.keras.layers.Dense(units=2)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.inputLayer(inputs)
+        x = self.full_layer_1(x)
+        x = self.full_layer_2(x)
+        x = self.full_layer_3(x)
+
+        """
+        TODO.
+        It the case we decided to use a single neuron output layer, Should we use a Softmax activation function?
+        
+        The output of the NN is passed through a Softmax activation function.
+        Softmax assigns decimal probabilities to each class in a multi-class problem.
+        """
+        return x, tf.keras.activations.softmax(x)
