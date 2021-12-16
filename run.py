9149a1cb3361a41272a448eccfea6651207cdc18
Debo790 <andrea.debeni@studenti.unitn.it>
2021-12-16 22:04:03 +0100

    updated for gpu test

--- run.py
+++ run.py
@@ -23,23 +23,23 @@ def gpu_check():
     """
     Select the computational core: CPU or GPU.
     """
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            tf.config.experimental.set_memory_growth(gpus[0], True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
     if tf.test.gpu_device_name():
         print(f'Default GPU device: {tf.test.gpu_device_name()}')
     else:
         CPU_brand = cpuinfo.get_cpu_info()['brand_raw']
         print(f'No GPU found, let\'s use CPU: {CPU_brand}')
         os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+    
 
 
 def main():
